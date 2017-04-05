69f87abaca1cefd7e38a79c2848d2b454166e9b4
harry-tallbelt <harry-tallbelt@users.noreply.github.com>
2017-04-05 07:56:32 +0300

    Commited the progress so far.

---
+++ search.py
@@ -0,0 +1,123 @@
+import datetime
+import asyncio
+from concurrent.futures import ProcessPoolExecutor # TODO: Why exacly not thread pool?
+import numpy as np
+from scipy.integrate import odeint
+from tsanalysis import EndBehaviour, determine_end_behaviour
+
+
+THREADS_COUNT = 2
+COMMON_LOG_FILE_NAME = 'general.log'
+CYCLES_LOG_FILE_NAME = 'cycles.log'
+STRANGE_ATTRACTORS_LOG_FILE_NAME = 'strange-attrators.log'
+
+
+# Legend for N_dot:
+# -----------------------------------------------------------------------
+# | a[0]      | a[1]    | a[2] | a[3] | a[4] | a[5]  | a[6]      | a[7] |
+# |-----------|---------|------|------|------|-------|-----------|------|
+# | epsilon_1 | gamma_1 |  d   |  L   |  K   | alpha | epsilon_3 |  k   |
+# -----------------------------------------------------------------------
+
+def N_dot(N, t, a):
+    N_dot_1 = (a[0] - a[1] * N[0]) * N[0] - a[2] * (N[0] - N[1])
+    N_dot_2 = a[0] * N[1] * (N[1] - a[3]) * (a[4] - N[1]) / a[4] \
+            - a[5] * N[1] * N[2] + a[2] * (N[0] - N[1])
+    N_dot_3 = -a[6] * N[2] + a[7] * a[5] * N[1] * N[2]
+    return np.array([N_dot_1, N_dot_2, N_dot_3])
+
+
+
+def analyse_random_parameters(param_max, param_min, initial_states, time_interval):
+    param_range = (param_max - param_min)
+    params = param_min + np.random.rand(param_count) * param_range
+    fixed_params_N_dot = lambda N, t: N_dot(N, t, params)
+    end_behaviours = []
+    
+    for N_init in initial_states:
+        time_series = odeint(fixed_params_N_dot, N_init, time_interval)
+        end_behaviour = determine_end_behaviour(time_series)
+        end_behaviours.append(end_behaviour)
+    
+    return params, end_behaviours
+
+
+async def start_search_chain(param_max, param_min, initial_states, time_interval, loop, pool):
+    params, end_behaviours = await loop.run_in_executor(
+        pool, analyse_random_parameters,
+        param_max, param_min, initial_states, time_interval
+    )
+    # TODO: Err.. but do we have to synchronise these lines?
+    # This would be the case if the event loop queue isn't thread safe.
+    # If so, what is the point of using the event loop anyway?
+    loop.put_nowait(lambda: on_new_results(params, initial_states, end_behaviours))
+    loop.put_nowait(start_search_chain)
+
+
+# Keeping track of total number of systems in console.
+_total, _cycles, _strange = 0, 0, 0
+
+def on_new_results(params, initial_states, end_behaviours):
+    assert(len(initial_states) == len(end_behaviours))
+    
+    # TODO: Rewrite it in three loops so we only
+    # have three file opennings instead of n to 2n.
+    for initial_state, end_behaviour in zip(initial_states, end_behaviours):
+        time_string = str(datetime.datetime.now()).split('.')[0]
+        log_entity = {
+            'time': time_string,
+            'end_behaviour': end_behaviour,
+            'parameters': parameters,
+            'initial_state': initial_state
+        }
+        with open(COMMON_LOG_FILE_NAME, 'w+') as common_log:
+            common_log.write(str(log_entity))
+        _total += 1
+
+        # Don't need that for behaviour-specific logs.
+        del log_entity['end_behaviour']
+        
+        if end_behaviour == EndBehaviour.LIMIT_CYCLE:
+            with open(CYCLES_LOG_FILE_NAME, 'w+') as cycle_log:
+                cycle_log.write(str(log_entity))
+            _cycles += 1
+        
+        elif end_behaviour == EndBehaviour.STRANGE_ATTRACTOR:
+            with open(STRANGE_ATTRACTORS_LOG_FILE_NAME, 'w+') as strange_log:
+                strange_log.write(str(log_entity))
+            _strange += 1
+    
+    # This is intentionaly outside the loop.
+    # Console IO isn't fast and we don't need update that frequent.
+    print(f'\rSystems in total: {_total}; cycles: {_cycles}, strange attractors: {_strange}.')
+
+
+# TODO: MAN, just use threads and a mutex
+# on the only function dealing with IO.
+# I BEG YOU.
+def search():
+    loop = asyncio.get_event_loop()
+    pool = ProcessPoolExecutor(THREADS_COUNT)
+    
+    params_min, params_max = 0.1, 100
+    params_count = 8
+    
+    # You don't want start at origin as it is always
+    # an equilibrium point (small populations die out).
+    interval = np.arange(0.1, 1 + 0.01, 0.3)
+    initial_states = [[x, y, z]
+        for x in interval for y in interval for z in interval]
+    time_interval = np.arange(0, 100, 0.1)
+
+    start_search = lambda: start_search_chain(
+        params_max, params_min,
+        initial_states, time_interval,
+        loop, pool
+    )
+    searches = [start_search() for _ in range(THREADS_COUNT)]
+    loop.run_until_complete(searches)
+
+
+
+if __name__ == '__main__':
+    search()
---
+++ tsanalysis.py
@@ -0,0 +1,133 @@
+from enum import Enum
+from numpy.linalg import norm as np_norm
+
+
+class EndBehaviour(Enum):
+    LIMIT_POINT = 0
+    LIMIT_CYCLE = 1
+    INFINITY = 2
+    STRANGE_ATTRACTOR = 3
+
+
+DEFAULD_CONFIDENCE_COUNT = 20
+DEFAULT_CLOSE_DISTANCE = 0.001
+
+
+def determine_end_behaviour(time_series,
+                            norm=np_norm,
+                            n=DEFAULD_CONFIDENCE_COUNT,
+                            eps=DEFAULT_CLOSE_DISTANCE):
+    """
+    Determines the end behaviour of the given time series.
+    Args:
+        time_series: An array-like of vectors - array-likes
+            of the same size.
+        
+        norm: The norm, used to calculate vector distances.
+            Defaults to np.linalg.norm.
+        
+        n: The function considers `n` last vectors of time_series.
+            Defaults to DEFAULD_CONFIDENCE_COUNT.
+
+        eps: The close enough distance between two vectors.
+            Defaults to DEFAULT_CLOSE_DISTANCE.
+    Returns:
+        One of the EndBehaviour enum values.
+    
+    Raises:
+        ValueError: if the time series is too short for the choosen n.
+    """
+    if goes_to_limit_point(time_series, norm, n, eps):
+        return EndBehaviour.LIMIT_POINT
+
+    if goes_to_infinity(time_series, norm, n, eps):
+        return EndBehaviour.INFINITY
+    
+    if goes_to_limit_cycle(time_series, norm, n, eps):
+        return EndBehaviour.LIMIT_CYCLE
+    # If it is none of above,
+    # it must be a strange attrator.
+    return EndBehaviour.STRANGE_ATTRACTOR
+
+
+
+def goes_to_limit_point(time_series,
+                        norm=np_norm,
+                        n=DEFAULD_CONFIDENCE_COUNT,
+                        eps=DEFAULT_CLOSE_DISTANCE):
+    # Take the last point of the time series ...
+    success_count = 0
+    reversed_time_series = reversed(time_series)
+    limit_point = next(reversed_time_series)
+    # ... and check that n last points
+    # are close enought to it.
+    for v in reversed_time_series:
+        if norm(limit_point - v) > eps:
+            return False
+        success_count += 1
+        next_vector = v
+        if success_count == n:
+            return True
+    # The exception is raised if n > len(time_series).
+    raise ValueError('The time series is too short for '
+                   + f'the choosen confidence count n={n}.')
+
+
+
+def goes_to_infinity(time_series,
+                     norm=np_norm,
+                     n=DEFAULD_CONFIDENCE_COUNT):
+    success_count = 0
+    reversed_time_series = reversed(time_series)
+    # Find the distance between the last two points ...
+    next_vector = next(reversed_time_series)
+    current_vector = next(reversed_time_series)
+    next_distance = norm(next_vector - current_vector)
+    next_vector = current_vector
+    # ... and make sure it only decreases
+    # (as time decreases) for n last points.
+    for current_vector in reversed_time_series:
+        current_distance = norm(next_vector - current_vector)
+        if current_distance >= next_distance:
+            return False
+        success_count += 1
+        next_vector = current_vector
+        next_distance = current_distance
+        if success_count == n:
+            return True
+    # The exception is raised if n > len(time_series).
+    raise ValueError('The time series is too short for '
+                    + f'the choosen confidence count n={n}.')
+
+
+
+def goes_to_limit_cycle(time_series,
+                        norm=np_norm,
+                        n=DEFAULD_CONFIDENCE_COUNT,
+                        eps=DEFAULT_CLOSE_DISTANCE):
+    success_count = 0
+    # For each point p1 (from the end)
+    for i in range(len(time_series), 0, -1):
+        # For each point p2 before it
+        for j in range(i - 1, 0, -1):
+            # If p1 and p2 close enough,
+            # they might be a part of a cycle.
+            if norm(time_series[i] - time_series[j]) < eps:
+                # Suppose they are a part of cycle with
+                # a period of their distance in time series.
+                cycle_flag, period = True, i - j
+                # Walk backwards with this period and check
+                # if all the points will be close enough.
+                curr = time_series[j]
+                for k in range(j - period, 0, -period):
+                    next, curr = curr, time_series[k]
+                    if norm(curr - next) > eps:
+                        cycle_flag = False
+                        break
+                # If they are, consider it a good
+                # evidence for a limit cycle.
+                if cycle_flag:
+                    success_count += 1
+    # If we return False here, it might be
+    # that we just didn't have enough information.
+    return success_count >= n
